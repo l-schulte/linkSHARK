daa3ede11f16cdfda36e4cff99e4989905b42bb8
Steffen Herbold <herbold@cs.uni-goettingen.de>
2018-12-06 10:52:52 +0100

    remove depenceny links

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@ setup(
     version='2.0.1',
     description='Issue linking for smartSHARK.',
     install_requires=['mongoengine', 'pymongo', 'pycoshark>=1.0.21'],
-    dependency_links=['git+https://github.com/smartshark/pycoSHARK.git@1.0.21#egg=pycoshark-1.0.21'],
     author='sherbold',
     author_email='herbold@cs.uni-goettingen.de',
     url='https://github.com/smartshark/linkSHARK',
